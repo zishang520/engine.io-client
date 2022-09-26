a08b786edf5adb20e1498e5a70207fef55c0636d
Luoyy <zishang520@gmail.com>
2022-09-26 17:54:59 +0800

    back

--- engine/transport.go
+++ engine/transport.go
@@ -1,1 +1,60 @@
 package engine
+
+type TransportError struct {
+	description any
+	context     any
+	errorType   string //"TransportError"
+}
+
+type CloseDetails struct {
+	description string
+	context     any
+}
+
+type Transport struct {
+	opts           SocketOptions
+	supportsBinary bool
+	query          any
+	readyState     string
+	writable       bool
+	socket         any
+	setTimeoutFn   setTimeout
+}
+
+// Transport abstract constructor.
+func NewTransport(opts any) {}
+
+// Emits an error.
+func (t *Transport) onError(reason string, description any, context any) {
+
+}
+
+// Opens the transport.
+func (t *Transport) open() {}
+
+// Closes the transport.
+func (t *Transport) Close() *Transport {}
+
+// Sends multiple packets.
+func (t *Transport) Send(packets any) {}
+
+// Called upon open
+func (t *Transport) onOpen() {}
+
+// Called with data.
+func (t *Transport) onData(data RawData) {}
+
+// Called with a decoded packet.
+func (t *Transport) onPacket(packet Packet) {}
+
+// Called upon close.
+func (t *Transport) onClose(details CloseDetails) {}
+func (t *Transport) doOpen() any {
+
+}
+func (t *Transport) doClose() any {
+
+}
+func (t *Transport) write(packets any) any {
+
+}
