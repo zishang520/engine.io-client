7c58536ff6402b9e6e9453df840a4acf868c375d
Luoyy <zishang520@gmail.com>
2022-10-27 18:10:03 +0800

    back

--- config/socket-options.go
+++ config/socket-options.go
@@ -8,6 +8,88 @@ import (
 	"github.com/zishang520/engine.io/utils"
 )
 
+type SocketOptionsInterface interface {
+	Host() string
+	GetRawHost() *string
+	SetHost(string)
+
+	Hostname() string
+	GetRawHostname() *string
+	SetHostname(string)
+
+	Secure() bool
+	GetRawSecure() *bool
+	SetSecure(bool)
+
+	Port() string
+	GetRawPort() *string
+	SetPort(string)
+
+	Query() *utils.ParameterBag
+	GetRawQuery() *utils.ParameterBag
+	SetQuery(*utils.ParameterBag)
+
+	Upgrade() bool
+	GetRawUpgrade() *bool
+	SetUpgrade(bool)
+
+	ForceBase64() bool
+	GetRawForceBase64() *bool
+	SetForceBase64(bool)
+
+	TimestampParam() string
+	GetRawTimestampParam() *string
+	SetTimestampParam(string)
+
+	TimestampRequests() bool
+	GetRawTimestampRequests() *bool
+	SetTimestampRequests(bool)
+
+	Transports() *types.Set[string]
+	GetRawTransports() *types.Set[string]
+	SetTransports(*types.Set[string])
+
+	RememberUpgrade() bool
+	GetRawRememberUpgrade() *bool
+	SetRememberUpgrade(bool)
+
+	OnlyBinaryUpgrades() bool
+	GetRawOnlyBinaryUpgrades() *bool
+	SetOnlyBinaryUpgrades(bool)
+
+	RequestTimeout() time.Duration
+	GetRawRequestTimeout() *time.Duration
+	SetRequestTimeout(time.Duration)
+
+	TransportOptions() map[string]*SocketOptions
+	GetRawTransportOptions() map[string]*SocketOptions
+	SetTransportOptions(map[string]*SocketOptions)
+
+	TLSClientConfig() *tls.Config
+	GetRawTLSClientConfig() *tls.Config
+	SetTLSClientConfig(*tls.Config)
+
+	ExtraHeaders() map[string]string
+	GetRawExtraHeaders() map[string]string
+	SetExtraHeaders(map[string]string)
+
+	CloseOnBeforeunload() bool
+	GetRawCloseOnBeforeunload() *bool
+	SetCloseOnBeforeunload(bool)
+
+	PerMessageDeflate() *PerMessageDeflate
+	GetRawPerMessageDeflate() *PerMessageDeflate
+	SetPerMessageDeflate(*PerMessageDeflate)
+
+	Path() string
+	GetRawPath() *string
+	SetPath(string)
+
+	Protocols() []string
+	GetRawProtocols() []string
+	SetProtocols([]string)
+}
+
 type PerMessageDeflate struct {
 	Threshold int
 }
@@ -101,3 +183,89 @@ type SocketOptions struct {
 	// @default []string
 	Protocols []string
 }
+
+func (s *SocketOptions) Host() string {
+	if s.host == nil {
+		return ""
+	}
+
+	return *s.host
+}
+func (s *SocketOptions) GetRawHost() *string {}
+func (s *SocketOptions) SetHost(string)      {}
+
+func (s *SocketOptions) Hostname() string        {}
+func (s *SocketOptions) GetRawHostname() *string {}
+func (s *SocketOptions) SetHostname(string)      {}
+
+func (s *SocketOptions) Secure() bool        {}
+func (s *SocketOptions) GetRawSecure() *bool {}
+func (s *SocketOptions) SetSecure(bool)      {}
+
+func (s *SocketOptions) Port() string        {}
+func (s *SocketOptions) GetRawPort() *string {}
+func (s *SocketOptions) SetPort(string)      {}
+
+func (s *SocketOptions) Query() *utils.ParameterBag       {}
+func (s *SocketOptions) GetRawQuery() *utils.ParameterBag {}
+func (s *SocketOptions) SetQuery(*utils.ParameterBag)     {}
+
+func (s *SocketOptions) Upgrade() bool        {}
+func (s *SocketOptions) GetRawUpgrade() *bool {}
+func (s *SocketOptions) SetUpgrade(bool)      {}
+
+func (s *SocketOptions) ForceBase64() bool        {}
+func (s *SocketOptions) GetRawForceBase64() *bool {}
+func (s *SocketOptions) SetForceBase64(bool)      {}
+
+func (s *SocketOptions) TimestampParam() string        {}
+func (s *SocketOptions) GetRawTimestampParam() *string {}
+func (s *SocketOptions) SetTimestampParam(string)      {}
+
+func (s *SocketOptions) TimestampRequests() bool        {}
+func (s *SocketOptions) GetRawTimestampRequests() *bool {}
+func (s *SocketOptions) SetTimestampRequests(bool)      {}
+
+func (s *SocketOptions) Transports() *types.Set[string]       {}
+func (s *SocketOptions) GetRawTransports() *types.Set[string] {}
+func (s *SocketOptions) SetTransports(*types.Set[string])     {}
+
+func (s *SocketOptions) RememberUpgrade() bool        {}
+func (s *SocketOptions) GetRawRememberUpgrade() *bool {}
+func (s *SocketOptions) SetRememberUpgrade(bool)      {}
+
+func (s *SocketOptions) OnlyBinaryUpgrades() bool        {}
+func (s *SocketOptions) GetRawOnlyBinaryUpgrades() *bool {}
+func (s *SocketOptions) SetOnlyBinaryUpgrades(bool)      {}
+
+func (s *SocketOptions) RequestTimeout() time.Duration        {}
+func (s *SocketOptions) GetRawRequestTimeout() *time.Duration {}
+func (s *SocketOptions) SetRequestTimeout(time.Duration)      {}
+
+func (s *SocketOptions) TransportOptions() map[string]*SocketOptions       {}
+func (s *SocketOptions) GetRawTransportOptions() map[string]*SocketOptions {}
+func (s *SocketOptions) SetTransportOptions(map[string]*SocketOptions)     {}
+
+func (s *SocketOptions) TLSClientConfig() *tls.Config       {}
+func (s *SocketOptions) GetRawTLSClientConfig() *tls.Config {}
+func (s *SocketOptions) SetTLSClientConfig(*tls.Config)     {}
+
+func (s *SocketOptions) ExtraHeaders() map[string]string       {}
+func (s *SocketOptions) GetRawExtraHeaders() map[string]string {}
+func (s *SocketOptions) SetExtraHeaders(map[string]string)     {}
+
+func (s *SocketOptions) CloseOnBeforeunload() bool        {}
+func (s *SocketOptions) GetRawCloseOnBeforeunload() *bool {}
+func (s *SocketOptions) SetCloseOnBeforeunload(bool)      {}
+
+func (s *SocketOptions) PerMessageDeflate() *PerMessageDeflate       {}
+func (s *SocketOptions) GetRawPerMessageDeflate() *PerMessageDeflate {}
+func (s *SocketOptions) SetPerMessageDeflate(*PerMessageDeflate)     {}
+
+func (s *SocketOptions) Path() string        {}
+func (s *SocketOptions) GetRawPath() *string {}
+func (s *SocketOptions) SetPath(string)      {}
+
+func (s *SocketOptions) Protocols() []string       {}
+func (s *SocketOptions) GetRawProtocols() []string {}
+func (s *SocketOptions) SetProtocols([]string)     {}
